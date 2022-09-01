993628ebeb118d814f445f37599988f882fb7eb7
Kringelum2 <p.kringelum@gmail.com>
2022-09-01 18:58:55 +0200

    Add project files.

---
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using WPFUI.Models;
+
+namespace WPFUI.ViewModels
+{
+    internal class ShellViewModel : Conductor<object>
+    {
+        private string _firstName = "Tim";
+        private string _lastName;
+        private PersonModel _selectedPerson;
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+
+        public ShellViewModel()
+        {
+            People.Add(new PersonModel { FirstName = "Tim", LastName = "Corey" });
+            People.Add(new PersonModel { FirstName = "Bill", LastName = "Jones" });
+            People.Add(new PersonModel { FirstName = "Sue", LastName = "Storm" });
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+
+        public string LastName
+        {
+            get 
+            { 
+                return _lastName; 
+            }
+            set 
+            { 
+                _lastName = value; 
+                NotifyOfPropertyChange(nameof(LastName));
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+        
+
+        public BindableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { _people = value; }
+        }
+
+
+        public PersonModel SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                _selectedPerson = value;
+                NotifyOfPropertyChange(() => SelectedPerson);
+            }
+        }
+
+        public bool CanClearText(string firstName, string lastName)
+        { 
+        if (String.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    public void ClearText(string firstName, string lastName)
+        {
+            FirstName = "";
+            LastName = "";
+        }
+
+        public void LoadPageOne()
+        {
+            ActivateItemAsync(new FirstChildViewModel());
+        }
+
+        public void LoadPageTwo()
+        {
+            ActivateItemAsync(new SecondChildViewModel());
+        }
+    }
+}
